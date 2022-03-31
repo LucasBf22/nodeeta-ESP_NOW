28e6e0d1f1ffa36dc493b3f6df5c9f5d69a71da4
Lucas Batista da Fonseca <lucasbf09@yahoo.com.br>
2022-03-31 20:38:44 -0300

    Tirei o mac address

--- Teste-esp_now/NodeETA_deio-bomba/NodeETA_deio-bomba.ino
+++ Teste-esp_now/NodeETA_deio-bomba/NodeETA_deio-bomba.ino
@@ -7,7 +7,7 @@ EnergyMonitor emon;  // cria o objeto p/ interagir com o sensor
 
 long lastMsg = 0;
 
-uint8_t broadcastAddress[] = {0x84,0xCC,0xA8,0x7A,0xC4,0x70};
+uint8_t broadcastAddress[] = {0x%02,0x%02,0x%02,0x%02,0x%02,0x%02};
 
 typedef struct struct_message {
   int id; // ID unico para cada esp.
